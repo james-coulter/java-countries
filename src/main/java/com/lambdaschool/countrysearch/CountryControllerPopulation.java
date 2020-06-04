4ecd69b2683bdbaaa2ceb4ff494d835d5637ea60
james-coulter <95coulter@gmail.com>
2020-06-04 15:18:24 -0600

    Finally figured out how to sort min

--- src/main/java/com/lambdaschool/countrysearch/CountryControllerPopulation.java
+++ src/main/java/com/lambdaschool/countrysearch/CountryControllerPopulation.java
@@ -1,10 +1,38 @@
 package com.lambdaschool.countrysearch;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
 
 @RestController
 @RequestMapping("/population")
 public class CountryControllerPopulation {
+
+    @GetMapping(value = "/size/{people}", produces = {"application/json"})
+    public ResponseEntity<?> countryPopGreaterEqual(@PathVariable Long people) {
+
+        List<Country> countries = CountrysearchApplication.ourCountryList.findCountries(c -> c.getPopulation() >= people);
+        countries.sort((c1, c2) -> c1.getCountryName().compareToIgnoreCase(c2.getCountryName()));
+        return new ResponseEntity<>(countries, HttpStatus.OK);
+    }
+
+//    @GetMapping(value = "/size/min", produces = {"application/json"})
+//    public ResponseEntity<?> countrySmallestPop() {
+//        void countries = CountrysearchApplication.ourCountryList.countryList.sort(Comparator.comparingLong(Country::getPopulation));
+//        return ResponseEntity<>(countries.get(0), HttpStatus.OK);
+//    }
+
+    @GetMapping("/min")
+    public ResponseEntity<?> getCountriesPopMin() {
+        CountrysearchApplication.ourCountryList.countryList.sort(Comparator.comparingLong(Country::getPopulation));
+        Country countries = CountrysearchApplication.ourCountryList.countryList.get(0);
+        return new ResponseEntity<>(countries, HttpStatus.OK);
+    }
 }
