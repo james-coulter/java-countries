3e4017df86bd925582910ea3c4946c3dd5b33a36
james-coulter <95coulter@gmail.com>
2020-06-04 14:06:01 -0600

    First endpoint working! Not broken yet, yay! *party*

--- src/main/java/com/lambdaschool/countrysearch/CountryControllerNames.java
+++ src/main/java/com/lambdaschool/countrysearch/CountryControllerNames.java
@@ -1,5 +1,8 @@
 package com.lambdaschool.countrysearch;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/names")
 public class CountryControllerNames {
 
+    @GetMapping(value = "/all", produces = {"application/json"})
+    public ResponseEntity<?> getCountries() {
+        CountrysearchApplication.ourCountryList.countryList
+                .sort((c1, c2) -> c1.getCountryName().compareToIgnoreCase(c2.getCountryName()));
+        return new ResponseEntity<>(CountrysearchApplication.ourCountryList.countryList, HttpStatus.OK);
+    }
 
 }
